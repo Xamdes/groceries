49ac2659a76b0f83909e8a56f65555ca837d9b06
Jean Jia and Steven Colburn <jean@>
2018-06-04 14:03:05 -0700

    lists sort then gets uppercased

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
+var listOfItems = [];
+var listOfSortedItems = [];
+
 $(function(){
-  
+  $("#organized-list").hide();
+  $("#btn-add").click(function(event){
+    event.preventDefault();
+    $("#grocery-list").fadeIn();
+    $("#organized-list").fadeOut();
+    var input = $("input").val();
+    input = input.trim();
+    if(input!="")
+    {
+      listOfItems.push(input);
+      $("#grocery-list").append(wrapListTag(input));
+    }
+  });
+
+  $("#btn-submit").click(function(event){
+    event.preventDefault();
+    $("#grocery-list").text("");
+    $("#list").text("");
+    $("#grocery-list").hide();
+    listOfItems.sort();
+    listOfSortedItems = [];
+    listOfItems.forEach(function(item)
+    {
+      item = wrapListTag(item.toUpperCase());
+      listOfSortedItems.push(item);
+    });
+    $("#list").append(listOfSortedItems);
+    $("#organized-list").fadeIn();
+    listOfItems = [];
+
+  });
+
 });
 
 
